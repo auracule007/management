be0b9e3b3d316ce2f6ae678a41e4ecf8eb49ba28
auracule007 <pogooluwa12@gmail.com>
2024-01-27 13:46:26 +0100

    masters

--- management/settings.py
+++ management/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["3.87.139.171", "*"]
 # ALLOWED_HOSTS = []
