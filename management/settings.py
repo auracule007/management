ff4b103a8d0771bd92dfdbf22d848b3921c6eac8
auracule007 <pogooluwa12@gmail.com>
2024-01-28 10:57:51 +0100

    host port

--- management/settings.py
+++ management/settings.py
@@ -17,10 +17,10 @@ SECRET_KEY = "django-insecure-g^(k(ph4lghw3n@e9gw6_7vf)or=sf3h&8ix!&ln_03#7!m3t4
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "codedextersacademy.onrender.com"]
+PORT = int(os.environ.get("PORT", 8000))
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "codedextersacademy.onrender.com", "*"]
 # ALLOWED_HOSTS = []
-
+# codedextersacademy.onrender.com
 # Application definition
 
 INSTALLED_APPS = [
