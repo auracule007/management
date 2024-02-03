d6aa4cb1565798637f4626fc4483415df7db197d
auracule007 <pogooluwa12@gmail.com>
2024-02-03 15:11:36 +0100

    update push

--- management/settings.py
+++ management/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = "django-insecure-g^(k(ph4lghw3n@e9gw6_7vf)or=sf3h&8ix!&ln_03#7!m3t4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://codedexteracademy.onrender.com/","*"]
+ALLOWED_HOSTS = ["https://codedexteracademy.onrender.com","*"]
 # Application definition
 
 INSTALLED_APPS = [
