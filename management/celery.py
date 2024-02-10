d069d3baabed90e62fdc0f50da8fe50e3f111b49
auracule007 <pogooluwa12@gmail.com>
2024-02-10 12:01:07 +0100

    updated requirements

--- management/celery.py
+++ management/celery.py
@@ -4,12 +4,8 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'management.settings')
 
 app = Celery('management')
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(settings.INSTALLED_APPS)
-
-
-# app.config_from_object('django.conf:settings', namespace='CELERY')
-# app.autodiscover_tasks()
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
 
 
 @app.task(bind=True, ignore_result=True)
