6a7b1d8ae184914463d67c10a38bc9efd17c4373
fawemimo <f.owolabi81@gmail.com>
2024-02-12 09:49:00 +0100

    course tasks updated

--- api/tasks.py
+++ api/tasks.py
@@ -13,10 +13,17 @@ def check_course_start_date():
     today = date.today()
     courses = Courses.objects.filter(set_start_date=today, is_started=False)
     for course in courses:
-        if course.set_start_date is not None:
+        if course.set_start_date == today:
             course.is_started = True
             try:
                 course.save()
-                logger.info("Successfully update the instance")
+                logger.info("Successfully update the instance to True")
             except Exception as e:
                 logger.info("Failed:", e)
+        else: 
+            course.is_started = False
+            try:
+                course.save()
+                logger.info("Successfully update the instance to Falsenn             n")
+            except Exception as e:
+                logger.error("Failed:", e)
